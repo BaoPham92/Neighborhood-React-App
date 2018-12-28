3ea34640621527f9f9bc3718ed742118c12906fe
Cuza <baopham92@yahoo.com>
2018-12-28 14:32:01 -0500

    Clutter removed & map is now properly displaying.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import mapHelper from './Utilities/helper'
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    mapHelper.genScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBglNEQSJfyWYuoGeyTCS5IyCoawoPMR0s&v=3&callback=initMap');
+  }
+
   render() {
     return (
       <main className='App'>
 
-      {/* Element for map */}
-      <div id="map">
-      </div>
+        {/* Element for map */}
+        <div id="map">
+        </div>
 
       </main>
-    );
+    )
   }
 }
-
-export default App;
