8d43d84d13a966edffa72f8783ce31b2895beb36
Cuza <baopham92@yahoo.com>
2019-01-03 16:36:55 -0500

    Map component & UserWindow Template base layout created

--- src/App.js
+++ src/App.js
@@ -1,104 +1,25 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import './App.css'
+
+import UserWindow from './Components/UserWindow.js'
+import Map from './Components/Map.js'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      places: []
     }
-  }
-
-  componentDidMount() {
-    this.getPlaces()
-  }
-
-  // Generate map
-  renderMap = () => {
-    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBglNEQSJfyWYuoGeyTCS5IyCoawoPMR0s&v=3&callback=initMap')
-    window.initMap = this.initMap
-  }
-
-  // Configuration and calls for API from foursqaure.
-  getPlaces = () => {
-    const params = {
-      client_id: '4UATXVRF0X3ABP1LJFJ0BVAZMC4V2I3Q53CVGJFN4Z0Z0NJV',
-      client_secret: 'LDAMUDHRRP3S0L43N03EXJWFZLUNXH50DOZZALE5ASA3DVNJ',
-      v: '20180323',
-      near: `New York City, NY`,
-      query: 'coffee',
-      limit: 50,
-      radius: 5000
-    }
-
-    // axios API call for returning information based on criterias from (params) variable above.
-    const endPoint = 'https://api.foursquare.com/v2/venues/explore?' + new URLSearchParams(params);
-
-    axios.get(endPoint)
-      .then((response) => {
-        this.setState({
-          places: response.data.response.groups[0].items
-        }, this.renderMap())
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-
-  // Map Initialization for API call on Google Maps
-  initMap = () => {
-    var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 40.7413549, lng: -73.9980244 },
-      zoom: 15
-    })
-
-    // Popup window for when clicking map markers
-    var infoWindow = new window.google.maps.InfoWindow
-
-    // Loop iteration for creating markers based on state data
-    this.state.places.filter(data => {
-
-      // Information for infoWindow
-      var contentWindow = `${data.venue.name}`
-
-      var marker = new window.google.maps.Marker({
-        position: { lat: data.venue.location.lat, lng: data.venue.location.lng },
-        map: map,
-        animation: window.google.maps.Animation.DROP,
-        title: data.venue.name
-      })
-
-      marker.addListener('click', () => {
-
-        // Change window content
-        infoWindow.setContent(contentWindow)
-
-        // Opening window of marker
-        infoWindow.open(map, marker)
-      })
-    })
   }
 
   render() {
     return (
       <main className='App'>
 
-        {/* Element for map */}
-        <div id="map">
-        </div>
+        <UserWindow props={this.state}/>
+        <Map />
 
       </main>
     )
   }
-}
-
-// Create and generate script for the window & initMap function^
-function loadScript(url) {
-  let script = window.document.createElement("script")
-  script.src = url
-  script.defer = true
-  script.async = true
-  window.document.body.appendChild(script)
 }
---
+++ src/Components/UserWindow.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class UserWindow extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="user-window">
+
+                <div className="search-box">
+
+                </div>
+
+                <div className="results-window">
+
+                </div>
+
+            </div>
+        )
+    }
+}
