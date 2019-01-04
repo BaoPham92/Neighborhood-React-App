f0f4016944a4b495a18203776204adb7199334e6
Cuza <baopham92@yahoo.com>
2019-01-03 20:50:01 -0500

    Added base Route, context, and components to App.js & Index.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import './App.css'
 
+// Components 
 import UserWindow from './Components/UserWindow.js'
 import Map from './Components/Map.js'
+
+// Utility Components
+import myContext from './Utilities/Context.js'
+import ContextProvider from './Utilities/ContextProvider'
 
 export default class App extends Component {
   constructor(props) {
@@ -15,10 +21,22 @@ export default class App extends Component {
   render() {
     return (
       <main className='App'>
+        <ContextProvider>
 
-        <UserWindow props={this.state}/>
-        <Map />
+          <Route exact path={'/'}
+            render={() => (
+              <myContext.Consumer>
+                {propData => 
+                <div>
+                <Map {...propData} /> 
 
+                <UserWindow {...propData} />
+                </div>
+                }
+              </myContext.Consumer>
+          )}/>
+
+        </ContextProvider>
       </main>
     )
   }
