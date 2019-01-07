04836055a43f75dd51afcc9f2accdcf28ca4d9a3
Cuza <baopham92@yahoo.com>
2019-01-06 22:45:24 -0500

    Restrucutered old axios API call into ContextProvider

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -33,7 +33,7 @@ export default class Map extends Component {
 
                 < MyMapComponent
                     isMarkerShown
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBglNEQSJfyWYuoGeyTCS5IyCoawoPMR0s&v"
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBglNEQSJfyWYuoGeyTCS5IyCoawoPMR0s&v=3"
                     loadingElement={< div style={{ height: `100%` }} />}
                     containerElement={< div style={{ height: `400px` }} />}
                     mapElement={< div style={{ height: `100%` }} />}
--- src/Utilities/ContextProvider.js
+++ src/Utilities/ContextProvider.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react'
 import myContext from './Context'
 import escapeRegExp from 'escape-string-regexp'
+import axios from 'axios'
+
 
 export default class ContextProvider extends Component {
     constructor(props) {
         super()
 
         this.state = {
-            query: ''
+            query: '',
+            places: [],
+            getPlaces: () => {
+                const params = {
+                    client_id: '4UATXVRF0X3ABP1LJFJ0BVAZMC4V2I3Q53CVGJFN4Z0Z0NJV',
+                    client_secret: 'LDAMUDHRRP3S0L43N03EXJWFZLUNXH50DOZZALE5ASA3DVNJ',
+                    v: '20180323',
+                    near: `New York City, NY`,
+                    query: 'coffee',
+                    limit: 50,
+                    radius: 5000
+                }
+
+                // axios API call for returning information based on criterias from (params) variable above.
+                const endPoint = 'https://api.foursquare.com/v2/venues/explore?' + new URLSearchParams(params);
+
+                axios.get(endPoint)
+                    .then((response) => {
+                        console.log(response)
+                        this.setState({ places: response.data.response.groups[0].items })
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            },
         }
+    }
+
+    componentDidMount() {
+        this.state.getPlaces()
     }
 
     render() {
