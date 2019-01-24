1654b675cdfc66891b18263dce5a87a85f24509c
Cuza <baopham92@yahoo.com>
2019-01-24 01:22:12 -0500

    Updates

---
+++ precache-manifest.68cb837a6dcb75ab623f9542196cabaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bded9499b7abe2a0bbe1",
+    "url": "/Neighborhood-React-App/static/js/runtime~main.bded9499.js"
+  },
+  {
+    "revision": "78a5e4e0499adcc146d2",
+    "url": "/Neighborhood-React-App/static/js/main.78a5e4e0.chunk.js"
+  },
+  {
+    "revision": "7d7f97bb54f7d5760af6",
+    "url": "/Neighborhood-React-App/static/js/1.7d7f97bb.chunk.js"
+  },
+  {
+    "revision": "78a5e4e0499adcc146d2",
+    "url": "/Neighborhood-React-App/static/css/main.240b49e6.chunk.css"
+  },
+  {
+    "revision": "525d76b5ed65a6d94e2a11240e9e7375",
+    "url": "/Neighborhood-React-App/index.html"
+  }
+];
